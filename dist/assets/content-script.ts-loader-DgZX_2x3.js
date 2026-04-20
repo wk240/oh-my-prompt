82f5919ccdcd891c6d1da7823796c679de702181
潘文康 <512710628@qq.com>
2026-04-20 15:07:16 +0800

    chore: update dist build artifacts

--- dist/assets/content-script.ts-loader-DgZX_2x3.js
+++ dist/assets/content-script.ts-loader-DgZX_2x3.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/content-script.ts-PHllm6J3.js")
+      chrome.runtime.getURL("assets/content-script.ts-cMPTqjUQ.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
